6a974459d8f44ce2b562ab3d19c58bff46e5bd4a
Samuel Dzib <samueldzibsads@gmail.com>
2020-09-28 21:29:47 -0500

    building backend socket

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+//APP express
+const app = express();
+
+
+
+//Node server
+const server = require('http').createServer(app);
+
+module.exports.io = require('socket.io')(server);
+
+require('./sockets/socket');
+
+//PATH PUBLIC   
+const publicPath = path.resolve(__dirname, 'public');
+app.use(express.static(publicPath));
+
+server.listen(process.env.PORT, (err) => {
+    if (err) throw new Error(err);
+    console.log('servidor corriendo en puerto');
+});
